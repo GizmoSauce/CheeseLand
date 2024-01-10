8e97ea874bf7f43eec3383cf4c6051b184000abf
GizmoSauce <153969530+GizmoSauce@users.noreply.github.com>
2024-01-10 16:54:18 -0500

    Re-Added files

--- cheeseClicker/cheese.js
+++ cheeseClicker/cheese.js
@@ -27,6 +27,36 @@ let cpc = 1;
 
 let cps = 0;
 
+const buildings = [
+    {
+        name: 'pan',
+        cost: document.querySelector(".panCost"),
+        trueCost: parseFloat(document.querySelector(".panCost").innerHTML),
+        cps: document.querySelector(".panCPS"),
+        trueCPS: parseFloat(document.querySelector(".panCPS").innerHTML),
+        amount: document.querySelector(".panAmount"),
+        costMultiplier: 1.25,
+    },
+    {
+        name: 'cow',
+        cost: document.querySelector(".cowCost"),
+        trueCost: parseFloat(document.querySelector(".cowCost").innerHTML),
+        cps: document.querySelector(".cowCPS"),
+        trueCPS: parseFloat(document.querySelector(".cowCPS").innerHTML),
+        amount: document.querySelector(".cowAmount"),
+        costMultiplier: 1.25,
+    },
+    {
+        name: 'fromagerie',
+        cost: document.querySelector(".fromagerieCost"),
+        trueCost: parseFloat(document.querySelector(".fromagerieCost").innerHTML),
+        cps: document.querySelector(".fromagerieCPS"),
+        trueCPS: parseFloat(document.querySelector(".fromagerieCPS").innerHTML),
+        amount: document.querySelector(".fromagerieAmount"),
+        costMultiplier: 1.25,
+    },
+]
+
 function incrementCheese(event) {
     cheese.innerHTML =  Math.round(cheeseAmount += cpc);
 
@@ -48,6 +78,25 @@ const timeout = (div) => [
         div.remove()
     }, 800)
 ]
+
+function buyBuilding(building) {
+    const mb = buildings.find((u) => {
+        if (u.name === building) return u
+    })
+
+    if (cheeseAmount >= mb.trueCost) {
+        cheese.innerHTML = Math.round(cheeseAmount -= mb.trueCost);
+
+        mb.amount.innerHTML ++
+
+        mb.trueCPS = parseFloat((mb.trueCPS).toFixed(2))
+        mb.cps.innerHTML = mb.trueCPS
+        cps += mb.trueCPS
+
+        mb.trueCost *= mb.costMultiplier;
+        mb.cost.innerHTML = Math.round(mb.trueCost)
+    }
+}
 
 function buyPan() {
     if (cheeseAmount >= truePanCost) {
