fee234c23f97a8fba197552b6fdb12170e347614
GizmoSauce <153969530+GizmoSauce@users.noreply.github.com>
2024-01-11 21:46:53 -0500

    Add files via upload

--- cheeseClicker/cheese.js
+++ cheeseClicker/cheese.js
@@ -5,10 +5,22 @@ let cheeseImageContainer = document.querySelector(".cheeseImageContainer")
 
 let cpsStat = document.querySelector(".cpsStat")
 
-let cpc = 1;
-
 let cps = 0;
 
+let cpc;
+
+if (cps > 2) {
+    cpc = cps * 0.5;
+} else {
+    cpc = 1;
+}
+
+const bgm = new Audio('../audio/code.mp3')
+bgm.volume = 0.1
+
+const upgradeSound = new Audio('../audio/upgrade.mp3')
+
+const upgradeInterval = [50, 100, 150, 200, 300, 400, 500, 1000]
 const buildings = [
     {
         name: 'pan',
@@ -17,6 +29,23 @@ const buildings = [
         cps: document.querySelector(".panCPS"),
         trueCPS: parseFloat(document.querySelector(".panCPS").innerHTML),
         amount: document.querySelector(".panAmount"),
+        upgrades: [
+            {
+                name: 'Hotter pans',
+                description: 'Make your pans hotter to cheddar double the cheese!',
+                multiplier: 2,
+            },
+            {
+                name: 'Even hotter pans',
+                description: 'Make your pans EVEN hotter to cheddar double the cheese!',
+                multiplier: 2,
+            },
+            {
+                name: 'Burning pans',
+                description: 'Get super heat-resiliant metal to cheddar DOUBLE the cheese!',
+                multiplier: 2,
+            },
+        ],
         costMultiplier: 1.25,
     },
     {
@@ -26,6 +55,23 @@ const buildings = [
         cps: document.querySelector(".cowCPS"),
         trueCPS: parseFloat(document.querySelector(".cowCPS").innerHTML),
         amount: document.querySelector(".cowAmount"),
+        upgrades: [
+            {
+                name: 'Grassier grass',
+                description: 'Give your cows grassier grass to double cheese production',
+                multiplier: 2,
+            },
+            {
+                name: 'Super! grass',
+                description: 'Give your cows the brand new Super! grass that, "helps your cattle produce DOUBLE the cheese!"',
+                multiplier: 2,
+            },
+            {
+                name: 'Nuclear grass',
+                description: 'I do not think your cows should be eating this. double cheese production',
+                multiplier: 2,
+            },
+        ],
         costMultiplier: 1.25,
     },
     {
@@ -35,6 +81,23 @@ const buildings = [
         cps: document.querySelector(".fromagerieCPS"),
         trueCPS: parseFloat(document.querySelector(".fromagerieCPS").innerHTML),
         amount: document.querySelector(".fromagerieAmount"),
+        upgrades: [
+            {
+                name: 'Bilboard ads',
+                description: 'Spread word of your fromageries on the highways! Certaintly doubles cheese!',
+                multiplier: 2,
+            },
+            {
+                name: 'Airplane banner advertising',
+                description: 'Spread word of your fromageries in the SKY! 2 times cheese production',
+                multiplier: 2,
+            },
+            {
+                name: 'Super Bowl ads',
+                description: 'The ultimate flex. Spread word of your fromageries in the Super Bowl! doubles cheese production ',
+                multiplier: 2,
+            },
+        ],
         costMultiplier: 1.25,
     },
     {
@@ -44,6 +107,23 @@ const buildings = [
         cps: document.querySelector(".wisconsinCPS"),
         trueCPS: parseFloat(document.querySelector(".wisconsinCPS").innerHTML),
         amount: document.querySelector(".wisconsinAmount"),
+        upgrades: [
+            {
+                name: 'Larger population',
+                description: 'Get more people to come to Wisconsin and produce doulble the cheese',
+                multiplier: 2,
+            },
+            {
+                name: 'Even larger population',
+                description: 'More people want to come to Wisconsin after seeing your cheese. 2 times cheese production',
+                multiplier: 2,
+            },
+            {
+                name: 'Secede from the nation',
+                description: 'Who needs america? Secede fromm the nation to start your own COUNTRY! double cheese production',
+                multiplier: 2,
+            },
+        ],
         costMultiplier: 1.25,
     },
 ]
@@ -55,6 +135,14 @@ const timeout = (div) => [
 ]
 
 function incrementCheese(event) {
+    if (cps > 2) {
+        cpc = cps * 0.3;
+    } else {
+        cpc = 1;
+    }
+
+    const clickSound = new Audio('../audio/click.mp3')
+    clickSound.play()
     cheese.innerHTML =  Math.round(cheeseAmount += cpc);
 
     const x = event.offsetX
@@ -62,7 +150,7 @@ function incrementCheese(event) {
 
     const div = document.createElement('div')
     div.innerHTML = `+${Math.round(cpc)}`
-    div.style.cssText = `color: white; position: absolute; top: ${y}px; left: ${x}px; font-size: 15px; pointer-events: none;`
+    div.style.cssText = `color: white; position: absolute; top: ${y}px; left: ${x}px; font-size: 15px; pointer-events: none; z-index: 15;`
     cheeseImageContainer.appendChild(div)
 
     div.classList.add('fadeUp')
@@ -70,14 +158,14 @@ function incrementCheese(event) {
     timeout(div)
 }
 
-
-
 function buyBuilding(building) {
     const mb = buildings.find((u) => {
         if (u.name === building) return u
     })
 
     if (cheeseAmount >= mb.trueCost) {
+        const buildingSound = new Audio('../audio/building.mp3')
+        buildingSound.play()
         cheese.innerHTML = Math.round(cheeseAmount -= mb.trueCost);
 
         mb.amount.innerHTML ++
@@ -129,8 +217,28 @@ function load() {
     cheese.innerHTML = Math.round(cheeseAmount)
 }
 
+function mute() {
+    if (bgm.volume > 0) {
+        bgm.volume = 0
+    }
+    else {
+        bgm.volume = 0.1
+    }
+}
+
 setInterval(() => {
+    bgm.play()
     cheeseAmount += cps / 10
     cheese.innerHTML = Math.round(cheeseAmount)
     cpsStat.innerHTML = Math.round(cps)
 }, 100)
+
+setInterval(() => {
+    save()
+}, 300000)
+
+function reset() {
+    localStorage.clear()
+    localStorage.save()
+    localStorage.load()
+}
